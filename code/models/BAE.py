fix mean auroc calculation in composed models

code/models/BAE.py
@@ -177,8 +177,8 @@ class BAE(IADModel):
             else:
                 aurocs[i] = torch.tensor(0.0, device=anomaly_scores.device)
                 average_precisions[i] = torch.tensor(0.0, anomaly_scores=self.device)
-        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).mean()
-        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).mean()
+        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).sum()
+        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).sum()
 
 
         metrics = {

code/models/CAE.py
@@ -233,8 +233,8 @@ class CAE(AEBase, IADModel):
             else:
                 aurocs[i] = torch.tensor(0.0, device=self.device)
                 average_precisions[i] = torch.tensor(0.0, device=self.device)
-        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).mean()
-        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).mean()
+        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).sum()
+        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).sum()
 
         self.log("val_accuracy", accuracy)
         self.log("val_precision", precision)
@@ -303,8 +303,8 @@ class CAE(AEBase, IADModel):
             else:
                 aurocs[i] = torch.tensor(0.0, device=self.device)
                 average_precisions[i] = torch.tensor(0.0, device=self.device)
-        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).mean()
-        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).mean()
+        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).sum()
+        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).sum()
 
         self.log("test_accuracy", accuracy)
         self.log("test_precision", precision)

code/models/KSAE.py
@@ -174,8 +174,8 @@ class KSAE(IADModel):
             else:
                 aurocs[i] = torch.tensor(0.0, device=self.device)
                 average_precisions[i] = torch.tensor(0.0, device=self.device)
-        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).mean()
-        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).mean()
+        auroc = (aurocs * cluster_sizes / cluster_sizes.sum()).sum()
+        average_precision = (average_precisions * cluster_sizes / cluster_sizes.sum()).sum()
 
         metrics = {
             "test_accuracy": accuracy,